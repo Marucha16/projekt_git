8d8315baa82ee31509604fe79e1502c508c36f2d
Marucha16 <mateusz.nycz17@interia.pl>
2025-06-09 01:23:51 +0200

    Update AES.sh

--- AES.sh
+++ AES.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 read -p "Podaj sciezke do pliku: " plik 
-zaszyfrowany="${PLIK}.aes"
+zaszyfrowany="${plik}.aes"
 read -sp "Podaj hasło do szyfrowania: " haslo
 openssl enc -aes-256-cbc -salt -in "$plik" -out "$zaszyfrowany" -pass pass:"$haslo"
 if [ $? -eq 0 ]; then
-  echo "Plik '$PLIK' został zaszyfrowany jako '$zaszyfrowany'"
+  echo "Plik '$plik' został zaszyfrowany jako '$zaszyfrowany'"
 else
   echo "Wystąpił błąd podczas szyfrowania."
 fi
